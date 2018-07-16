b4dd759a8fff29a7cd4edb6722f217da2dccc5f8
Sam Kennedy <eldersamkennedy@gmail.com>
2018-07-16 22:00:49 +0100

    Basic version working with raw output

--- driver.py
+++ driver.py
@@ -1,15 +1,22 @@
+from rtlsdr import RtlSdr
+from raw_data import RawData
+from router import Router
+from downlink_format_17_18 import DownlinkFormat17
+from utils import calc_lat_lon
+
+
 class Driver:
-    def __init__(self):
-        """
-        global_aircraft_data is a dictionary where the key is an aircraft ICAO code
-        and the value is a dictionary containing aircraft attributes (altitude, heading, etc.) mapped to values
+    """
+    global_aircraft_data is a dictionary where the key is an aircraft ICAO code
+    and the value is a dictionary containing aircraft attributes (altitude, heading, etc.) mapped to values
 
-        For example:
-        global_aircraft_data = {'84AB6F': {'altitude': 36000, 'heading': 120, 'callsign': 'EZY1825'}, '66BBFF': {..}}
-        """
-        self.global_aircraft_data = {}
+    For example:
+    global_aircraft_data = {'84AB6F': {'altitude': 36000, 'heading': 120, 'callsign': 'EZY1825'}, '66BBFF': {..}}
+    """
+    global_aircraft_data = {}
 
-    def run(self):
+    @staticmethod
+    def run():
         """
         This is the main routine:
         1) Capture data from SDR and add to a RawData object
@@ -19,5 +26,43 @@ class Driver:
         latitude and longitude
         5) Send global_aircraft_data to output handler
         """
+        RAW_SAMPLES = 512000
+        SAMPLES_PER_EXTRACTION = 4
+        FREQUENCY = 1090000000
+        GAIN = 297
+        SAMPLE_RATE = 2000000
 
+        sdr = RtlSdr()
+        sdr.sample_rate = SAMPLE_RATE
+        sdr.center_freq = FREQUENCY
+        sdr.gain = GAIN
 
+        Router.routing_table[17] = DownlinkFormat17.decode_packet
+        Router.routing_table[18] = DownlinkFormat17.decode_packet
+
+        while True:
+            raw_data = RawData()
+
+            for i in range(0, SAMPLES_PER_EXTRACTION):
+                raw_data.add_sample(sdr.read_samples(RAW_SAMPLES))
+
+            binary_packets = raw_data.get_binary_packets()
+            extracted_properties = Router.extract_data(binary_packets)
+
+            for icao in extracted_properties:
+                if icao in Driver.global_aircraft_data:
+                    Driver.global_aircraft_data[icao].update(extracted_properties[icao])
+                else:
+                    Driver.global_aircraft_data[icao] = extracted_properties[icao]
+
+            for icao in Driver.global_aircraft_data:
+                if all(key in Driver.global_aircraft_data[icao] for key in ['lat_even', 'lon_even', 'lat_odd', 'lon_odd']):
+                    position = calc_lat_lon(Driver.global_aircraft_data[icao])
+
+                    if position:
+                        Driver.global_aircraft_data[icao].update(position)
+
+            print(Driver.global_aircraft_data)
+
+if __name__ == '__main__':
+    Driver.run()
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from math import floor, cos, acos, pi
+
+
 def bin2int(binary_arr):
     binary_string = ''.join(str(x) for x in binary_arr)
     return int(binary_string, 2)
@@ -20,3 +23,62 @@ def crc(binary_packet, encode=False):
             result_arr[i+x] ^= generator[x]
 
     return bin2int(result_arr[-24:])
+
+
+def nl(lat):
+    if abs(lat) == 87:
+        return 2
+
+    if abs(lat) > 87:
+        return 1
+
+    num_zones = 15
+    cos_nz = cos(pi / (2.0 * num_zones))
+    cos_lat = cos((pi/180.0) * lat)
+    bottom = acos(1 - ((1 - cos_nz) / (cos_lat ** 2)))
+
+    return floor((2 * pi) / bottom)
+
+
+def mod(x, y):
+    return x - y * floor(x / y)
+
+
+def calc_lat_lon(aircraft):
+    j = floor(59 * aircraft['lat_even'] - 60 * aircraft['lat_odd'] + 0.5)
+
+    d_lat_even = 360.0 / 60.0
+    d_lat_odd = 360.0 / 59.0
+
+    lat_even = d_lat_even * (mod(j, 60) + aircraft['lat_even'])
+    lat_odd = d_lat_odd * (mod(j, 59) + aircraft['lat_odd'])
+
+    if lat_even >= 270:
+        lat_even -= 360
+
+    if lat_odd >= 270:
+        lat_odd -= 360
+
+    if aircraft['even_timestamp'] >= aircraft['odd_timestamp']:
+        latitude = lat_even
+    else:
+        latitude = lat_odd
+
+    if nl(lat_even) != nl(lat_odd):
+        return None
+
+    if aircraft['even_timestamp'] > aircraft['odd_timestamp']:
+        ni = max(nl(lat_even), 1)
+        d_lon = 360.0 / ni
+        m = floor(aircraft['lon_even'] * (nl(lat_even) - 1) - aircraft['lon_odd'] * nl(lat_even) + 0.5)
+        lon = d_lon * (mod(m, ni) + aircraft['lon_even'])
+    else:
+        ni = max(nl(lat_odd) - 1, 1)
+        d_lon = 360.0 / ni
+        m = floor(aircraft['lon_even'] * (nl(lat_odd) - 1) - aircraft['lon_odd'] * nl(lat_odd) + 0.5)
+        lon = d_lon * (mod(m, ni) + aircraft['lon_odd'])
+
+    if lon >= 180:
+        lon -= 360
+
+    return {'latitude': latitude, 'longitude': lon}
